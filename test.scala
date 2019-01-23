4726a8e3745f1683ada484dc7f4a6ad06f168056
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-01-23 11:38:23 +0000

    Try to get to 50 issues on the file

--- test.scala
+++ test.scala
@@ -1,71 +1,75 @@
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2var a=1
-var b=2
-var a=1
-var b=2var a=1
-var b=2
-var a=1
-var b=2
-var a=1
-var b=2
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
+var b=2;
+var a=1;
