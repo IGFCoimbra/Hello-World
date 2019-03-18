4288e2ab6ca06cb1112359fa515d0d4d36a9f3a9
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-03-18 14:10:00 +0000

    Update Java_file.java

--- Java_file.java
+++ Java_file.java
@@ -12,6 +12,46 @@ public class ErrorTests {
         this.str = str;
         this.testInt = testInteger;
     }
+    
+    public class ErrorTests {
+    private String str;
+    private final int testInt;
+
+    public ErrorTests(String str, int testInteger){
+        this.str = str;
+        this.testInt = testInteger;
+    }
+
+    public void testMethod(){
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println(str);
+        if (str.startsWith("test") == true) {
+            if(testInt == 0) {
+                System.out.println(testInt + str);
+            }
+        }
+
+        try (ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+                ByteArrayOutputStream stderr = new ByteArrayOutputStream()) {
+            stdout.close();
+            stderr.close();
+            System.out.println(stdout.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("command: ", e);
+        }
+
+
+        File fPath = new File("");
+        if (fPath.isDirectory()) {
+            File[] langPaths = fPath.listFiles();
+            if (langPaths.length > 0) {
+                System.out.println("null pointer problem");
+            }
+        }
+
+    }
+
 
     public void testMethod(){
         Runtime runtime = Runtime.getRuntime();
