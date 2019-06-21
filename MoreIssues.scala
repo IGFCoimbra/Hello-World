7a5c31fdfe8b876748846198ff98abcf7b259477
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-06-21 11:56:58 +0100

    Update MoreIssues.scala

--- MoreIssues.scala
+++ MoreIssues.scala
@@ -1,7 +1,7 @@
-var a=1;
-var b=2;
-var q=1;
-var w=2;
-var e=1;
-var r=2;
-var t=1;
+immutable val a=1;
+immutable val b=2;
+immutable val q=1;
+immutable val w=2;
+immutable val e=1;
+immutable val r=2;
+immutable val t=1;
