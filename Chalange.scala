b39eaa19f3342898e53d6989ddad47a40b307d24
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-08-06 15:13:08 +0100

    Update Chalange.scala

--- Chalange.scala
+++ Chalange.scala
@@ -17,10 +17,6 @@ case class Test(
   tag:             Option[String]        = None
 )
 
-implicit def converterTransactionDataFromPSApi(entry: Future[Either[ErrorResult, PaginatedSequence[TransactionDataApi]]])(implicit lang: Lang, executionCtx: ExecutionContext, ctx_credentials: ContextCredentials, ctx_m_info: ContextMInfo): Future[Either[Fail, PaginatedSequence[TransactionData]]] =
-    EntityConverter.converterFromApi[PaginatedSequence[TransactionDataApi], PaginatedSequence[TransactionData]](entry)(lang, executionCtx, ctx_m_info, api2transaction_data_ps)
-
-
  
  val format = Json.format[Transaction]
  val data_format = Jsonx.formatCaseClass[TransactionData]
