0c905994c58c111649dc1e8695d4f2ce47137067
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-08-06 16:51:32 +0100

    Update Chalange.scala

--- Chalange.scala
+++ Chalange.scala
@@ -1,26 +1,29 @@
-val a=1;
-var b=2;
+package Test
 
-def sqrt(x: Double) = {
-  def sqrtIter(guess: Double, x: Double): Double =
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
+class Testing {
+  val a = 1;
+  var b = 2;
 
-  def improve(guess: Double, x: Double) =
-    (guess + x / guess) / 2
+  def sqrt(x: Double) = {
+    def sqrtIter(guess: Double, x: Double): Double =
+      if (isGoodEnough(guess, x)) guess
+      else sqrtIter(improve(guess, x), x)
 
-  def isGoodEnough(guess: Double, x: Double) =
-    abs(square(guess) - x) < 0.001
+    def improve(guess: Double, x: Double) =
+      (guess + x / guess) / 2
 
-  sqrtIter(1.0, x)
+    def isGoodEnough(guess: Double, x: Double) =
+      abs(square(guess) - x) < 0.001
+
+    sqrtIter(1.0, x)
+  }
 }
 
 case class Test(
-  id:              String                = "",
-  Type:            String                = "",
-  issue_id:        String                = "",
-  issue_type:      String                = "",
-  recipient_id:    String                = "",
-  recipient_type:  String                = ""
-
-)
+                 id: String = "",
+                 Type: String = "",
+                 issue_id: String = "",
+                 issue_type: String = "",
+                 recipient_id: String = "",
+                 recipient_type: String = ""
+               )
