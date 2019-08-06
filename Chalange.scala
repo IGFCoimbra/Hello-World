dd93f55ebc0fd055e23f7698eb934a24a730a4c1
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-08-06 17:31:26 +0100

    Update Chalange.scala

--- Chalange.scala
+++ Chalange.scala
@@ -4,26 +4,28 @@ class Testing {
   val a = 1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111;
   var b = 2;
 
+  var b = 2;
+  
   def sqrt(x: Double) = {
     def sqrtIter(guess: Double, x: Double): Double =
       if (isGoodEnough(guess, x)) guess
       else sqrtIter(improve(guess, x), x)
-
+    
     def improve(guess: Double, x: Double) =
       (guess + x / guess) / 2
-
+    
     def isGoodEnough(guess: Double, x: Double) =
       abs(square(guess) - x) < 0.001
-
+    
     sqrtIter(1.0, x)
   }
 }
 
-case class Test(
+case class test(
                  id: String = "",
                  Type: String = "",
                  issue_id: String = "",
                  issue_type: String = "",
                  recipient_id: String = "",
                  recipient_type: String = ""
-               )
+        
