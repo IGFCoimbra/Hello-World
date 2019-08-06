76e174639fbb0f12de20953e6c29e33834314ecd
IGFCoimbra <36641997+IGFCoimbra@users.noreply.github.com>
2019-08-06 15:26:22 +0100

    Update Chalange.scala

--- Chalange.scala
+++ Chalange.scala
@@ -18,5 +18,36 @@ case class Test(
 )
 
  
- val format = Json.format[Transaction]
+object TransactionTransformation extends DateTransformation {
+
+  def api2transaction(transaction_api: TransactionApi): Transaction = {
+    val copy = CopyMacro.copyCaseClass[TransactionApi, Transaction]
+    val transaction: Transaction = copy(transaction_api, Transaction())
+
+    transaction.copy(
+      id = transaction_api.id,
+      fundraise_id = transaction_api.fundraise_id.getOrElse(""),
+      fundraise_type = transaction_api.fundraise_type.headOption.flatMap(FundraiseType.get(_)),
+      amount = NumberUtils.fromCents(transaction_api.amount),
+      fees = NumberUtils.fromCents(transaction_api.fees),
+      custom = transaction_api.custom
+    )
+  }
+
+  def api2transactionData(transaction_data_api: TransactionDataApi): TransactionData = {
+    val copy = CopyMacro.copyCaseClass[TransactionDataApi, TransactionData]
+    val transaction_data: TransactionData = copy(transaction_data_api, TransactionData())
+
+    transaction_data.copy(
+      id = transaction_data_api.id,
+      amount = transaction_data_api.amount.map(NumberUtils.fromCents),
+      fees = transaction_data_api.fees.map(NumberUtils.fromCents),
+      custom = transaction_data_api.custom
+    )
+  }
+
+  
+  lazy val format = Json.format[Transaction]
+  lazy val data_format = Jsonx.formatCaseClass[TransactionData]
+}
  val data_format = Jsonx.formatCaseClass[TransactionData]
